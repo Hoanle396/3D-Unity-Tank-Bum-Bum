0fb8e45a341f7c7c2033677ae3c0a18ea2c8cdfa
phamxuanhienkt <phamxuanhientk2002@gmail.com>
2024-03-25 14:37:50 +0700

    fix

---
+++ Assets/Scripts/UI/SliderScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _slider;
+
+    [SerializeField]
+    private AudioSource audio;
+    void Start()
+    {
+        _slider.onValueChanged.AddListener((v) => {
+            audio.volume = v / 100;
+        });
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
